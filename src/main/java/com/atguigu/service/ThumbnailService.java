f69e0b403af4774c72524ae45e2c73de10691832
ColdMoonlight <1020064691@qq.com>
2020-05-29 17:45:40 +0800

    update

--- src/main/java/com/atguigu/service/ThumbnailService.java
+++ src/main/java/com/atguigu/service/ThumbnailService.java
@@ -1,5 +1,7 @@
 package com.atguigu.service;
 
+import java.util.Calendar;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import net.coobird.thumbnailator.Thumbnails;
@@ -16,7 +18,7 @@ public class ThumbnailService {
 	 * @exception 查看用户信息是否存在
 	 * 
 	 * */
-	public String Thumbnail(CommonsMultipartFile file,String uploadPath,String realUploadUrl) {
+	public String Thumbnail(CommonsMultipartFile file,String uploadPath,String realUploadUrl,String typeName) {
 		
 		try {
 			String des = realUploadUrl+"/"+"thumb_"+file.getOriginalFilename();
@@ -27,6 +29,20 @@ public class ThumbnailService {
 		}
 		return uploadPath+"/"+"thumb_"+file.getOriginalFilename();
 		
+	}
+	
+    private static String getfilename(String cateid) {
+		Calendar c = Calendar.getInstance();//可以对每个时间域单独修改   对时间进行加减操作等
+		int year = c.get(Calendar.YEAR);
+		 int month = c.get(Calendar.MONTH);
+		int date = c.get(Calendar.DATE);
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		int minute = c.get(Calendar.MINUTE);   
+		int second = c.get(Calendar.SECOND);
+		System.out.println(year + "/" + month + "/" + date + " " +hour + ":" +minute + ":" + second);    
+		String newfilename = "cateid"+ cateid +"time"+date+hour+minute+second+".jpg";
+		System.out.println(newfilename);
+		return newfilename;
 	}
 	
 }
