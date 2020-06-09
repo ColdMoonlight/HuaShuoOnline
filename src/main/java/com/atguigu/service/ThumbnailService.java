bd58950f7bc9c868bb4b8dc5619981db0f8bd716
ColdMoonlight <1020064691@qq.com>
2020-06-09 09:34:19 +0800

    add uploadProDisImg

--- src/main/java/com/atguigu/service/ThumbnailService.java
+++ src/main/java/com/atguigu/service/ThumbnailService.java
@@ -8,7 +8,10 @@ import net.coobird.thumbnailator.Thumbnails;
 public class ThumbnailService {
 	
 	public static final int WIDTH = 200;
-	public static final int HEIGH = 200; 
+	public static final int HEIGH = 200;
+	
+	public static final int DisWIDTH = 50;
+	public static final int DisHEIGH = 50;
 	
 	/**
 	 * @author Shinelon
@@ -22,6 +25,20 @@ public class ThumbnailService {
 			String des = realUploadUrl+"/"+"thumb_"+imgName;//文件最终的路径
 //			String des = realUploadUrl+"/"+"thumb_"+file.getOriginalFilename();
 			Thumbnails.of(file.getInputStream()).scale(0.4f).toFile(des);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return uploadPath+"/"+"thumb_"+imgName;
+		
+	}
+	
+	public String ThumbnailProDiscount(CommonsMultipartFile file,String uploadPath,String realUploadUrl,String imgName) {
+		
+		try {
+			String des = realUploadUrl+"/"+"thumb_"+imgName;//文件最终的路径
+//			String des = realUploadUrl+"/"+"thumb_"+file.getOriginalFilename();
+			Thumbnails.of(file.getInputStream()).scale(DisWIDTH,DisHEIGH).toFile(des);
 
 		} catch (Exception e) {
 			e.printStackTrace();
