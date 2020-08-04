b30d39665f6e2b16049a78d87a598b6b525401ac
Administrator <Administrator@PC-202007311130>
2020-08-04 16:31:28 +0800

    update

--- src/main/java/com/atguigu/dao/MlbackAddCartViewDetailMapper.java
+++ src/main/java/com/atguigu/dao/MlbackAddCartViewDetailMapper.java
@@ -1,17 +1,14 @@
 package com.atguigu.dao;
 
+import java.util.List;
+
 import com.atguigu.bean.MlbackAddCartViewDetail;
 
 public interface MlbackAddCartViewDetailMapper {
+    
+    int insertSelective(MlbackAddCartViewDetail record);
+    
     int deleteByPrimaryKey(Integer addcartviewdetailId);
 
-    int insert(MlbackAddCartViewDetail record);
-
-    int insertSelective(MlbackAddCartViewDetail record);
-
-    MlbackAddCartViewDetail selectByPrimaryKey(Integer addcartviewdetailId);
-
-    int updateByPrimaryKeySelective(MlbackAddCartViewDetail record);
-
-    int updateByPrimaryKey(MlbackAddCartViewDetail record);
+	List<MlbackAddCartViewDetail> selectMlbackAddCartViewDetailByTimeAndActnum(MlbackAddCartViewDetail mlbackAddCartViewDetail);
 }
