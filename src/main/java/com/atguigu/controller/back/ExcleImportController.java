045620a4365a9286efa246f8631597e22ca54593
Administrator <Administrator@PC-202007311130>
2020-08-25 10:42:54 +0800

    update

--- src/main/java/com/atguigu/controller/back/ExcleImportController.java
+++ src/main/java/com/atguigu/controller/back/ExcleImportController.java
@@ -1,6 +1,8 @@
 package com.atguigu.controller.back;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
@@ -20,6 +22,7 @@ import com.atguigu.bean.MlfrontReview;
 import com.atguigu.service.DownPayCheckDateService;
 import com.atguigu.service.MlfrontPayInfoService;
 import com.atguigu.service.MlfrontReviewService;
+import com.atguigu.utils.DateUtil;
 
 @Controller
 @RequestMapping("/excleImport")
@@ -45,16 +48,73 @@ public class ExcleImportController {
 	}
 	
 	/**
-     * 人员信息导入
-     * @param conId
-     * @param code
+	 * 下载review模板
+	 * */
+	@RequestMapping(value="/exportReviewsImportDemo",method=RequestMethod.GET)
+	public void exportReviewsImportDemo(HttpServletResponse rep,HttpServletRequest res,HttpSession session){
+		
+		rep.setContentType("application/octet-stream");
+		String nowTime = DateUtil.strTime14();
+		rep.setHeader("Content-Disposition", "attachment;filename="+nowTime+"payinfoIf.xls");
+		
+		HSSFWorkbook wb = new HSSFWorkbook();
+		HSSFSheet sheet = wb.createSheet("sheet0");
+		HSSFRow row = sheet.createRow(0);
+		HSSFCell cell = row.createCell(0);
+		
+		cell.setCellValue("ReviewUname");
+	    cell = row.createCell(1);
+		cell.setCellValue("ReviewUimgurl");
+	    cell = row.createCell(2);
+	    cell.setCellValue("ReviewPid");
+	    cell = row.createCell(3);
+	    cell.setCellValue("ReviewCreatetime");
+	    cell = row.createCell(4);
+	    cell.setCellValue("ReviewMotifytime");
+	    cell = row.createCell(5);
+	    cell.setCellValue("ReviewPseoname");
+	    cell = row.createCell(6);
+	    cell.setCellValue("ReviewStatus");
+	    cell = row.createCell(7);
+	    cell.setCellValue("ReviewDetailstr");
+	    cell = row.createCell(8);
+	    cell.setCellValue("ReviewProstarnum");
+	    cell = row.createCell(9);
+	    cell.setCellValue("ReviewFrom");
+	    cell = row.createCell(10);
+	    
+        row = sheet.createRow(1);
+        row.createCell(0).setCellValue("MegaLookCustomer");//ReviewUname
+        row.createCell(1).setCellValue("https://www.megalook.com/static/upload/img/ReviewUImg/M.png");//ReviewUimgurl
+        row.createCell(2).setCellValue(252);//ReviewPid
+        row.createCell(3).setCellValue("2019-07-02 15:23:43");//ReviewCreatetime
+        row.createCell(4).setCellValue("2019-07-02 15:23:43");//ReviewMotifytime
+        row.createCell(5).setCellValue("613-Color-13x4-Body-Wave-Lace-Wig");//ReviewPseoname
+        row.createCell(6).setCellValue("0不展示/1展示");//ReviewStatus
+        row.createCell(7).setCellValue("i like megalook's hair,very like");//ReviewDetailstr
+        row.createCell(8).setCellValue("1/2/3/4/5星评论");//ReviewProstarnum
+        row.createCell(9).setCellValue("0-self/1-customer/2-ins瀑布流/3-ins首页");//ReviewFrom
+        
+		try {
+			OutputStream out =rep.getOutputStream();
+			wb.write(out);
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+     * inportReviews
      * @param request
      * @param response
      */
 	@RequestMapping(value="/inportReviews",method=RequestMethod.POST)
-	public void insertReviews(@RequestParam(value = "file", required = false) MultipartFile multipartFile,HttpServletRequest request,HttpServletResponse response,HttpSession session){
+	public void inportReviews(@RequestParam(value = "file", required = false) MultipartFile multipartFile,HttpServletRequest request,HttpServletResponse response,HttpSession session){
 		try {
 			InputStream is = multipartFile.getInputStream();
+			String nowTime = DateUtil.strTime14();
 			if(is!=null){
 				HSSFWorkbook wb = new HSSFWorkbook(is);
 				List<MlfrontReview> reviewList = new ArrayList<MlfrontReview>();
@@ -92,6 +152,8 @@ public class ExcleImportController {
 						if (getCell != null) {
 		                    getCell.setCellType(HSSFCell.CELL_TYPE_STRING);
 		                    reviewOne.setReviewMotifytime(getCell.getStringCellValue());
+		                    reviewOne.setReviewConfirmtime(nowTime);
+		                    reviewOne.setReviewSupercateidstr("1");
 		                }
 						getCell = row.getCell(5);
 						if (getCell != null) {
