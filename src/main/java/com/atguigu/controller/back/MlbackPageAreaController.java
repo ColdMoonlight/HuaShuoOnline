d37cf864ce5dc3354e835c6bcc67973e118dfcb4
Administrator <Administrator@PC-202007311130>
2020-10-14 11:28:55 +0800

    update

--- src/main/java/com/atguigu/controller/back/MlbackPageAreaController.java
+++ src/main/java/com/atguigu/controller/back/MlbackPageAreaController.java
@@ -1,11 +1,22 @@
 package com.atguigu.controller.back;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.atguigu.bean.MlbackActShowPro;
 import com.atguigu.bean.MlbackAdmin;
+import com.atguigu.bean.MlbackPageArea;
+import com.atguigu.common.Msg;
+import com.atguigu.service.MlbackPageAreaService;
 import com.atguigu.service.MlfrontUserService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 /**
  * MlbackPageArea
@@ -18,12 +29,15 @@ public class MlbackPageAreaController {
 	@Autowired
 	MlfrontUserService mlfrontUserService;
 	
+	@Autowired
+	MlbackPageAreaService mlbackPageAreaService;
+	
 	/**
 	 * zsh 201014
 	 * 首页后台控制台
 	 * */
-	@RequestMapping("/MlbackPageareaPage")
-	public String MlbackPageareaPage(HttpSession session) throws Exception{
+	@RequestMapping("/MlbackPageAreaPage")
+	public String MlbackPageAreaPage(HttpSession session) throws Exception{
 		
 		MlbackAdmin mlbackAdmin =(MlbackAdmin) session.getAttribute("AdminUser");
 		if(mlbackAdmin==null){
@@ -33,5 +47,19 @@ public class MlbackPageAreaController {
 			return "back/mlbackPageAreaPage";
 		}
 	}
-
+	
+	/**2.0	zsh 201014
+	 * 分类MlbackPageArea列表分页list数据
+	 * @param pn
+	 * @return
+	 */
+	@RequestMapping(value="/getMlbackPageAreaByPage")
+	@ResponseBody
+	public Msg getMlbackPageAreaByPage(@RequestParam(value = "pn", defaultValue = "1") Integer pn,HttpSession session) {
+		int PagNum = 20;
+		PageHelper.startPage(pn, PagNum);
+		List<MlbackPageArea> mlbackActShowProList = mlbackPageAreaService.selectMlbackPageAreaGetAll();
+		PageInfo page = new PageInfo(mlbackActShowProList, PagNum);
+		return Msg.success().add("pageInfo", page);
+	}
 }
---
+++ src/main/java/com/atguigu/service/MlbackPageAreaService.java
@@ -0,0 +1,67 @@
+package com.atguigu.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.atguigu.bean.MlbackPageArea;
+import com.atguigu.dao.MlbackPageAreaMapper;
+
+@Service
+public class MlbackPageAreaService {
+	
+	@Autowired
+	MlbackPageAreaMapper mlbackPageAreaMapper;
+	
+	/**
+	 * @author Shinelon
+	 * @param MlbackPageArea
+	 * @exception add方法用户信息是否存在
+	 * 
+	 * */
+	public int insertSelective(MlbackPageArea MlbackPageArea) {
+		int intReslut = mlbackPageAreaMapper.insertSelective(MlbackPageArea);
+		return intReslut;
+	}
+	
+	/**
+	 * @author Shinelon
+	 * @param 
+	 * @exception 删除本条信息
+	 * 
+	 * */
+	public int deleteByPrimaryKey(int pageareaId) {
+		int  intReslut = mlbackPageAreaMapper.deleteByPrimaryKey(pageareaId);
+		return intReslut;
+	}
+	
+	/**
+	 * @author Shinelon
+	 * @param 
+	 * @exception 更新本条信息
+	 * */
+	public int updateByPrimaryKeySelective(MlbackPageArea MlbackPageArea) {
+		int  intReslut = mlbackPageAreaMapper.updateByPrimaryKeySelective(MlbackPageArea);
+		return intReslut;
+	}
+
+	/**
+	 * @author Shinelon
+	 * @param MlbackPageArea
+	 * @exception 查看用户信息是否存在
+	 * */
+	public MlbackPageArea selectMlbackPageAreaById(MlbackPageArea MlbackPageArea) {
+		MlbackPageArea  MlbackPageAreaOne = mlbackPageAreaMapper.selectMlbackPageAreaById(MlbackPageArea);
+		return MlbackPageAreaOne;
+	}
+	
+	/**
+	 * @author Shinelon
+	 * @param 
+	 * @exception 查看全部用户信息
+	 * */
+	public List<MlbackPageArea> selectMlbackPageAreaGetAll() {
+		List<MlbackPageArea>  MlbackPageAreaList = mlbackPageAreaMapper.selectMlbackPageAreaGetAll();
+		return MlbackPageAreaList;
+	}
+
+}
