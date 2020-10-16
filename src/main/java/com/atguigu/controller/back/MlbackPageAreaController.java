51655ad0702da1d75ca6a993cf775177d4a56c4a
Administrator <Administrator@PC-202007311130>
2020-10-16 11:38:23 +0800

    update

--- src/main/java/com/atguigu/controller/back/MlbackPageAreaController.java
+++ src/main/java/com/atguigu/controller/back/MlbackPageAreaController.java
@@ -266,7 +266,7 @@ public class MlbackPageAreaController {
 							String productidStr ="";
 							Integer productidInt =0;
 							List<MlbackProduct> mlbackProductReqList = new ArrayList<MlbackProduct>();
-							//
+							//查询单独的信息
 							MlbackProduct mlbackProductResOne = new MlbackProduct();
 							for(int x=0;x<productidsStrArr.length;x++){
 								productidStr = productidsStrArr[x];
