990be1ed3ac4bf5d4e393044966fe29222017563
Administrator <Administrator@PC-202007311130>
2020-10-19 09:17:49 +0800

    update

--- src/main/java/com/atguigu/controller/back/MlbackPageAreaController.java
+++ src/main/java/com/atguigu/controller/back/MlbackPageAreaController.java
@@ -311,10 +311,11 @@ public class MlbackPageAreaController {
 								mlbackProductResList.add(mlbackProductResOne);
 							}
 						}
+						
 						for(MlbackProduct mlbackProductReqOne:mlbackProductResList){
 							
 							PageAreaDetail pageAreaDetailReturn = new PageAreaDetail();
-							//System.out.println("ProductId:"+mlbackProductReqOne.getProductId()+",ProductSeo:"+mlbackProductReqOne.getProductSeo());
+							//System.out.println("ProductId:"+mlbackProductReqOne.getProductId()+" ,ProductSeo:"+mlbackProductReqOne.getProductSeo());
 							pageAreaDetailReturn.setMlbackProduct(mlbackProductReqOne);
 							pageAreaDetailReturn.setPageAreaDetailIfinto(1);
 							pageAreaDetailReturn.setPageAreaDetailType(2);
