e64eba9c1cd63e30ed80fe6e7e24c6f690b126ac
Administrator <Administrator@PC-202007311130>
2020-11-02 15:42:42 +0800

    add 批发客户

---
+++ src/main/java/com/atguigu/bean/CustomerWholesale.java
@@ -0,0 +1,112 @@
+package com.atguigu.bean;
+
+public class CustomerWholesale {
+	
+    private Integer wholesaleId;
+
+    private String wholesaleCustomerName;
+
+    private String wholesaleCustomerEmail;
+
+    private String wholesaleCustomerCountry;
+
+    private String wholesaleCustomerWatsapp;
+
+    private String wholesaleCustomerMessage;
+
+    private String wholesaleCreatetime;
+
+    private String wholesaleMotifytime;
+
+    public Integer getWholesaleId() {
+        return wholesaleId;
+    }
+
+    public void setWholesaleId(Integer wholesaleId) {
+        this.wholesaleId = wholesaleId;
+    }
+
+    public String getWholesaleCustomerName() {
+        return wholesaleCustomerName;
+    }
+
+    public void setWholesaleCustomerName(String wholesaleCustomerName) {
+        this.wholesaleCustomerName = wholesaleCustomerName == null ? null : wholesaleCustomerName.trim();
+    }
+
+    public String getWholesaleCustomerEmail() {
+        return wholesaleCustomerEmail;
+    }
+
+    public void setWholesaleCustomerEmail(String wholesaleCustomerEmail) {
+        this.wholesaleCustomerEmail = wholesaleCustomerEmail == null ? null : wholesaleCustomerEmail.trim();
+    }
+
+    public String getWholesaleCustomerCountry() {
+        return wholesaleCustomerCountry;
+    }
+
+    public void setWholesaleCustomerCountry(String wholesaleCustomerCountry) {
+        this.wholesaleCustomerCountry = wholesaleCustomerCountry == null ? null : wholesaleCustomerCountry.trim();
+    }
+
+    public String getWholesaleCustomerWatsapp() {
+        return wholesaleCustomerWatsapp;
+    }
+
+    public void setWholesaleCustomerWatsapp(String wholesaleCustomerWatsapp) {
+        this.wholesaleCustomerWatsapp = wholesaleCustomerWatsapp == null ? null : wholesaleCustomerWatsapp.trim();
+    }
+
+    public String getWholesaleCustomerMessage() {
+        return wholesaleCustomerMessage;
+    }
+
+    public void setWholesaleCustomerMessage(String wholesaleCustomerMessage) {
+        this.wholesaleCustomerMessage = wholesaleCustomerMessage == null ? null : wholesaleCustomerMessage.trim();
+    }
+
+    public String getWholesaleCreatetime() {
+        return wholesaleCreatetime;
+    }
+
+    public void setWholesaleCreatetime(String wholesaleCreatetime) {
+        this.wholesaleCreatetime = wholesaleCreatetime == null ? null : wholesaleCreatetime.trim();
+    }
+
+    public String getWholesaleMotifytime() {
+        return wholesaleMotifytime;
+    }
+
+    public void setWholesaleMotifytime(String wholesaleMotifytime) {
+        this.wholesaleMotifytime = wholesaleMotifytime == null ? null : wholesaleMotifytime.trim();
+    }
+
+	public CustomerWholesale() {
+		super();
+	}
+
+	public CustomerWholesale(Integer wholesaleId, String wholesaleCustomerName, String wholesaleCustomerEmail,
+			String wholesaleCustomerCountry, String wholesaleCustomerWatsapp, String wholesaleCustomerMessage,
+			String wholesaleCreatetime, String wholesaleMotifytime) {
+		super();
+		this.wholesaleId = wholesaleId;
+		this.wholesaleCustomerName = wholesaleCustomerName;
+		this.wholesaleCustomerEmail = wholesaleCustomerEmail;
+		this.wholesaleCustomerCountry = wholesaleCustomerCountry;
+		this.wholesaleCustomerWatsapp = wholesaleCustomerWatsapp;
+		this.wholesaleCustomerMessage = wholesaleCustomerMessage;
+		this.wholesaleCreatetime = wholesaleCreatetime;
+		this.wholesaleMotifytime = wholesaleMotifytime;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerWholesale [wholesaleId=" + wholesaleId + ", wholesaleCustomerName=" + wholesaleCustomerName
+				+ ", wholesaleCustomerEmail=" + wholesaleCustomerEmail + ", wholesaleCustomerCountry="
+				+ wholesaleCustomerCountry + ", wholesaleCustomerWatsapp=" + wholesaleCustomerWatsapp
+				+ ", wholesaleCustomerMessage=" + wholesaleCustomerMessage + ", wholesaleCreatetime="
+				+ wholesaleCreatetime + ", wholesaleMotifytime=" + wholesaleMotifytime + "]";
+	}
+    
+}
