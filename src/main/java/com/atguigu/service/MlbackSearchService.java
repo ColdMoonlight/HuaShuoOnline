6656412e56bc58ab618be86251166f3664173b60
Administrator <Administrator@SKY-20210317VKK>
2021-05-13 10:35:37 +0800

    update

--- src/main/java/com/atguigu/service/MlbackSearchService.java
+++ src/main/java/com/atguigu/service/MlbackSearchService.java
@@ -1,11 +1,8 @@
 package com.atguigu.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.atguigu.bean.MlbackAddCheakoutViewDetail;
 import com.atguigu.bean.MlbackSearch;
 import com.atguigu.dao.MlbackSearchMapper;
 
