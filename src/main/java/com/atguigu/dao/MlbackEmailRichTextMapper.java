27c9621c9cd55f486061979ae55d986a40921e1d
Administrator <Administrator@SKY-20210317VKK>
2021-06-05 10:17:28 +0800

    update

--- src/main/java/com/atguigu/dao/MlbackEmailRichTextMapper.java
+++ src/main/java/com/atguigu/dao/MlbackEmailRichTextMapper.java
@@ -5,19 +5,20 @@ import java.util.List;
 import com.atguigu.bean.MlbackEmailRichText;
 
 public interface MlbackEmailRichTextMapper {
-    int deleteByPrimaryKey(Integer emailrichtextId);
 
     int insert(MlbackEmailRichText record);
-
+    //add
     int insertSelective(MlbackEmailRichText record);
-
+	//delete
+    int deleteByPrimaryKey(Integer emailrichtextId);
+	//update
+    int updateByPrimaryKeySelective(MlbackEmailRichText record);
+	//select
     MlbackEmailRichText selectByPrimaryKey(Integer emailrichtextId);
 
-    int updateByPrimaryKeySelective(MlbackEmailRichText record);
-
     int updateByPrimaryKey(MlbackEmailRichText record);
-
+	//select列表
 	List<MlbackEmailRichText> selectMlbackEmailRichTextGetAll();
-
+	//通过产品名查询select列表
 	List<MlbackEmailRichText> selectMlbackEmailRichTextListByName(MlbackEmailRichText mlbackEmailRichTextReq);
 }
